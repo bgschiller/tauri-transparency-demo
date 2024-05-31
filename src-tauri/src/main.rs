663cfc032e3e25dd23e4bca12d9113813bc38598
Brian Schiller <brian.schiller@grammarly.com>
2024-05-31 14:53:29 -0700

    demo changes

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,16 +1,36 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+#[cfg(target_os = "macos")]
+use objc::{msg_send, sel, sel_impl};
+use tauri::Manager;
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+// The web code will tell us when the mouse is positioned over an element that should
+// be clickable. At that point, it will call this function to tell us to stop ignoring
+// mouse events.
+#[tauri::command]
+fn set_ignore_mouse_events(app_handle: tauri::AppHandle, ignore: bool, forward: bool) {
+    let main_window = app_handle.get_webview_window("main").unwrap();
+    let _ = main_window.with_webview(move |webview| {
+        #[cfg(target_os = "macos")]
+        unsafe {
+            let () = msg_send![webview.ns_window(), setIgnoresMouseEvents:ignore];
+            let () = msg_send![webview.ns_window(), setAcceptsMouseMovedEvents:forward];
+        }
+    });
+    return ();
+}
+
 fn main() {
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![greet, set_ignore_mouse_events])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/App.tsx
+++ src/App.tsx
@@ -12,20 +12,91 @@ function App() {
     setGreetMsg(await invoke("greet", { name }));
   }
 
+  function captureMouseEvents() {
+    invoke("set_ignore_mouse_events", { ignore: false, forward: false });
+  }
+
+  function releaseMouseEvents() {
+    invoke("set_ignore_mouse_events", { ignore: true, forward: true });
+  }
+
+  const colors = [
+    { background: "black", text: "white" },
+    { background: "white", text: "black" },
+    { background: "red", text: "white" },
+    { background: "green", text: "white" },
+    { background: "blue", text: "white" },
+    { background: "yellow", text: "black" },
+    { background: "purple", text: "white" },
+    { background: "orange", text: "black" },
+    { background: "pink", text: "black" },
+    { background: "brown", text: "white" },
+    { background: "gray", text: "white" },
+  ];
+  const [colorIndex, setColorIndex] = useState(0);
+  const color = colors[colorIndex % colors.length];
+
   return (
-    <div className="container">
+    <div
+      className="container"
+      // Note! these ones are backwards from the rest
+      onMouseEnter={releaseMouseEvents}
+      onMouseLeave={captureMouseEvents}
+    >
       <h1>Welcome to Tauri!</h1>
 
       <div className="row">
         <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
+          <img
+            src="/vite.svg"
+            className="logo vite"
+            alt="Vite logo"
+            onMouseEnter={captureMouseEvents}
+            onMouseLeave={releaseMouseEvents}
+          />
         </a>
         <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
+          <img
+            src="/tauri.svg"
+            className="logo tauri"
+            alt="Tauri logo"
+            onMouseEnter={captureMouseEvents}
+            onMouseLeave={releaseMouseEvents}
+          />
         </a>
         <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img
+            src={reactLogo}
+            className="logo react"
+            alt="React logo"
+            onMouseEnter={captureMouseEvents}
+            onMouseLeave={releaseMouseEvents}
+          />
         </a>
+        <svg width="600" height="200" xmlns="http://www.w3.org/2000/svg">
+          <path
+            id="wavyPath"
+            d="M10,100 C50,150 150,50 200,100 S350,150 400,100 S550,50 590,100"
+            fill="transparent"
+            stroke={color.background}
+            strokeWidth={40}
+            onClick={() => setColorIndex(colorIndex + 1)}
+            onMouseEnter={captureMouseEvents}
+            onMouseLeave={releaseMouseEvents}
+          />
+
+          <text
+            font-family="Arial"
+            font-size="20"
+            fill={color.text}
+            dy="5"
+            className="wavy-text"
+          >
+            <textPath href="#wavyPath" startOffset="5%">
+              works with non-rectangular regions!
+            </textPath>
+          </text>
+        </svg>
       </div>
 
       <p>Click on the Tauri, Vite, and React logos to learn more.</p>
@@ -36,6 +107,8 @@ function App() {
           e.preventDefault();
           greet();
         }}
+        onMouseEnter={captureMouseEvents}
+        onMouseLeave={releaseMouseEvents}
       >
         <input
           id="greet-input"
